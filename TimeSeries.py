17a13d6ae0c8cba747f71d60d425951dd637c2e0
swathi0710 <117807966+swathi0710@users.noreply.github.com>
2023-02-09 21:41:34 +0530

    Add files via upload

---
+++ TimeSeries.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import math
+
+print("all good!")
