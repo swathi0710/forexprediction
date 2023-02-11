cf2d9e6abc1dfbf1d8d4254737c4e0c52b3da1ca
swathi0710 <117807966+swathi0710@users.noreply.github.com>
2023-02-11 15:49:20 +0530

    Update TimeSeries.py

--- TimeSeries.py
+++ TimeSeries.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 url='https://drive.google.com/file/d/18-4rLXTR2B-KVsGTMkngcYQUwhnM0Rod/view?usp=sharing'
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
@@ -50,6 +52,21 @@ st.line_chart(weekly["close"])
 
 df_close = weekly['close']
 
-
+def test_stationarity(timeseries):
+    rolmean = timeseries.rolling(52).mean()
+    rolstd = timeseries.rolling(52).std()
+    
+    datf=pd.DataFrame({"y1":timeseries,"y2":rolmean,"y3":rolstd,"x":timeseries.index})
+    
+    st.line_chart(datf[[['x', 'y1', 'y2','y3']]
+    st.write("Results of dickey fuller test")
+    adft = adfuller(timeseries,autolag='AIC')
+    # output for dft will give us without defining what the values are.
+    #hence we manually write what values does it explains using a for loop
+    output = pd.Series(adft[0:4],index=['Test Statistics','p-value','No. of lags used','Number of observations used'])
+    for key,values in adft[4].items():
+        output['critical value (%s)'%key] =  values
+    st.write(output)
+test_stationarity(df_close)
 
 
