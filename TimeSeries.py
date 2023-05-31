930b785110726894a4d1779a007520c3cdac995d
swathi0710 <117807966+swathi0710@users.noreply.github.com>
2023-05-31 16:15:12 +0530

    Update old TimeSeries.py

--- TimeSeries.py
+++ TimeSeries.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.arima.model import ARIMA
-from prophet.forecaster import Prophet
+from prophet import Prophet
 from pmdarima.arima import auto_arima
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import plotly.express as px
