618217f989d2f5eb3a53a1a692a21f5bd1d90a18
Joshua Salako <58732290+joshsalako@users.noreply.github.com>
2024-10-15 07:32:21 +0100

    changed experimental to cache

--- TimeSeries.py
+++ TimeSeries.py
@@ -18,7 +18,7 @@ from datetime import datetime, timedelta
 import plotly.offline as po
 import main_functions as mfn
 
-@st.experimental_memo
+@st.cache
 def gen():
     data = mfn.load_data()
     data["A"]=[str(a).split("/")[0] for a in data["slug"]]
